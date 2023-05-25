a0cdf168606b4c51184c0a97fe5d8526f59bdfdc
Marc Cadiz <111546505+Y0shiroo@users.noreply.github.com>
2023-05-25 11:34:33 +0800

    Update U_2SpyPlane.java

--- Attendance/U_2SpyPlane.java
+++ Attendance/U_2SpyPlane.java
@@ -1,14 +1,13 @@
-
+//Used inheritance to use the method STOP() from the Vehicle
 public class U_2SpyPlane extends Vehicle {
 String Wingspan = "105ft";
 
 void Fly() {
-	System.out.println("Taking Off");
+System.out.println("Taking Off");
 }
-
+//Used overriding for method STOP()
 @Override
 void Stop() {
-	System.out.println("System Override");
-	super.Stop();
+super.Stop();
 }
 }
