79380dcf676dd5547c10cd4401268d1c8e685cce
Marc Cadiz <111546505+Y0shiroo@users.noreply.github.com>
2023-05-27 22:53:18 +0800

    Create table.java

---
+++ table.java
@@ -0,0 +1,219 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class table {
+
+	private JFrame frame;
+	private JTextField TFposcode;
+	private JTextField TFjobtitle;
+	private JTextField TFrespon;
+	private JTextField TFsalary;
+	private JTable table;
+	private JScrollPane scrollPane;
+	DefaultTableModel model;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					table window = new table();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public table() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 700, 550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(192, 192, 192));
+		panel.setForeground(new Color(0, 0, 0));
+		panel.setBounds(0, 0, 686, 513);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblPosCode = new JLabel("Position Code:");
+		lblPosCode.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPosCode.setBounds(10, 57, 105, 35);
+		panel.add(lblPosCode);
+		
+		JLabel lblJobTitle = new JLabel("Job Title:");
+		lblJobTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJobTitle.setBounds(10, 102, 105, 35);
+		panel.add(lblJobTitle);
+		
+		JLabel lblResponsibilities = new JLabel(" Responsibilities:");
+		lblResponsibilities.setHorizontalAlignment(SwingConstants.CENTER);
+		lblResponsibilities.setBounds(10, 147, 105, 35);
+		panel.add(lblResponsibilities);
+		
+		JLabel lblSalary = new JLabel("Salary:");
+		lblSalary.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSalary.setBounds(10, 192, 105, 35);
+		panel.add(lblSalary);
+		
+		TFposcode = new JTextField();
+		TFposcode.setBounds(139, 65, 134, 19);
+		panel.add(TFposcode);
+		TFposcode.setColumns(10);
+		
+		TFjobtitle = new JTextField();
+		TFjobtitle.setColumns(10);
+		TFjobtitle.setBounds(139, 110, 134, 19);
+		panel.add(TFjobtitle);
+		
+		TFrespon = new JTextField();
+		TFrespon.setColumns(10);
+		TFrespon.setBounds(139, 155, 134, 19);
+		panel.add(TFrespon);
+		
+		TFsalary = new JTextField();
+		TFsalary.setColumns(10);
+		TFsalary.setBounds(139, 200, 134, 19);
+		panel.add(TFsalary);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(296, 40, 360, 430);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int i = table.getSelectedRow();
+				TFposcode.setText(model.getValueAt(i,0).toString());
+				TFjobtitle.setText(model.getValueAt(i,1).toString());
+				TFrespon.setText(model.getValueAt(i,2).toString());
+				TFsalary.setText(model.getValueAt(i,3).toString());
+			}
+		});
+		table.setBackground(new Color(165, 216, 220));
+		model = new DefaultTableModel();
+		Object[] column = {"Position Code","Job Title", "Responsibilities", "Salary"};
+		final Object[] row = new Object[4];
+		model.setColumnIdentifiers(column);
+		table.setModel(model);
+		scrollPane.setViewportView(table);
+		
+		JButton btnAdd = new JButton("ADD");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(TFposcode.getText().equals("")|| TFjobtitle.getText().equals("")|| TFrespon.getText().equals("")|| TFsalary.getText().equals(""))
+				{
+					JOptionPane.showMessageDialog(null,"Please Complete Filling Out The Information");
+				} else {
+				row[0] = TFposcode.getText();
+				row[1] = TFjobtitle.getText();
+				row[2] = TFrespon.getText();
+				row[3] = TFsalary.getText();
+				model.addRow(row);
+				TFposcode.setText("");
+				TFjobtitle.setText("");
+				TFrespon.setText("");
+				TFsalary.setText("");
+				}
+			}
+		});
+		btnAdd.setBounds(46, 284, 90, 35);
+		panel.add(btnAdd);
+		
+		JButton btnUpdate = new JButton("UPDATE");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			int i = table.getSelectedRow();
+			if(i>=0) {
+			model.setValueAt(TFposcode.getText(), i, 0);
+			model.setValueAt(TFjobtitle.getText(), i, 1);
+			model.setValueAt(TFrespon.getText(), i, 2);
+			model.setValueAt(TFsalary.getText(), i, 3);
+			JOptionPane.showMessageDialog(null,"Updated Successfully!");
+			TFposcode.setText("");
+			TFjobtitle.setText("");
+			TFrespon.setText("");
+			TFsalary.setText("");
+			}else{
+				JOptionPane.showMessageDialog(null,"Select A Row To Update");
+			}
+			}
+		});
+		btnUpdate.setBounds(169, 284, 90, 35);
+		panel.add(btnUpdate);
+		
+		JButton btnDelete = new JButton("DELETE");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			int i=table.getSelectedRow();
+			if(i>=0) {
+			model.removeRow(i);
+			JOptionPane.showMessageDialog(null,"Deleted Successfully!");
+			TFposcode.setText("");
+			TFjobtitle.setText("");
+			TFrespon.setText("");
+			TFsalary.setText("");
+			} else {
+			JOptionPane.showMessageDialog(null,"Select A Row To Delete");
+			}
+			}
+		});
+		btnDelete.setBounds(46, 341, 90, 35);
+		panel.add(btnDelete);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i = table.getSelectedRow();
+				TFposcode.setText(model.getValueAt(i, 0).toString());
+				TFjobtitle.setText(model.getValueAt(i, 1).toString());
+				TFrespon.setText(model.getValueAt(i, 2).toString());
+				TFsalary.setText(model.getValueAt(i, 3).toString());
+			}
+		});
+		scrollPane.setBounds(296, 40, 360, 430);
+		panel.add(scrollPane);
+		
+		JButton btnClear = new JButton("CLEAR");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TFposcode.setText("");
+				TFjobtitle.setText("");
+				TFrespon.setText("");
+				TFsalary.setText("");
+			}
+		});
+		btnClear.setBounds(169, 341, 90, 35);
+		panel.add(btnClear);
+	}
+}
