753a6d0b8dbf7f51a6f6e8b45fc4ad8e6d8805b4
Marc Cadiz <111546505+Y0shiroo@users.noreply.github.com>
2023-06-03 14:43:04 +0800

    Create PageOne.java

---
+++ LongExam1/PageOne.java
@@ -0,0 +1,104 @@
+package L1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class PageOne extends JFrame {
+
+	private JPanel PageOne;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PageOne frame = new PageOne();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PageOne() {
+		setTitle("PORTFOLIO");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 705, 726);
+		PageOne = new JPanel();
+		PageOne.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(PageOne);
+		PageOne.setLayout(null);
+		
+		JButton btnAboutMe = new JButton("ABOUT ME");
+		btnAboutMe.setFont(new Font("Bell MT", Font.BOLD, 13));
+		btnAboutMe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			AboutMe ABT = new AboutMe();
+			ABT.setVisible(true);
+			dispose();
+			}
+		});
+		btnAboutMe.setBounds(170, 555, 120, 61);
+		PageOne.add(btnAboutMe);
+		
+		JButton btnInterests = new JButton("INTERESTS");
+		btnInterests.setFont(new Font("Bell MT", Font.BOLD, 13));
+		btnInterests.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Interests INT = new Interests();
+				INT.setVisible(true);
+				dispose();
+			}
+		});
+		btnInterests.setBounds(561, 555, 120, 61);
+		PageOne.add(btnInterests);
+		
+		JButton btnEduc = new JButton("EDUCATION");
+		btnEduc.setFont(new Font("Bell MT", Font.BOLD, 13));
+		btnEduc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Educ EDU = new Educ();
+				EDU.setVisible(true);
+				dispose();
+			}
+		});
+		btnEduc.setBounds(430, 555, 120, 61);
+		PageOne.add(btnEduc);
+		
+		JButton btnSkills = new JButton("SKILL SET");
+		btnSkills.setFont(new Font("Bell MT", Font.BOLD, 13));
+		btnSkills.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Skills SKL = new Skills();
+				SKL.setVisible(true);
+				dispose();
+			}
+		});
+		btnSkills.setFont(new Font("Bell MT", Font.BOLD, 13));
+		btnSkills.setBounds(300, 555, 120, 61);
+		PageOne.add(btnSkills);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Marc Cadiz\\AppData\\Local\\Microsoft\\Windows\\INetCache\\IE\\KO42U40N\\TITLE_PAGE[1].png"));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 0, 700, 700);
+		PageOne.add(lblNewLabel);
+	}
+}
